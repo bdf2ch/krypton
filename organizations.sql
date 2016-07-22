94d654a15fb60aa959c693353a78ce85d5002720
bdf2ch <kolu0897@nw.mrsksevzap.ru>
2016-07-22 20:29:39 +0300

    temp

---
+++ organizations.sql
@@ -0,0 +1,32 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.3
+-- https://www.phpmyadmin.net/
+--
+-- Хост: localhost
+-- Время создания: Июл 22 2016 г., 17:29
+-- Версия сервера: 5.5.23
+-- Версия PHP: 5.5.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `krypton`
+--
+
+--
+-- Дамп данных таблицы `organizations`
+--
+
+INSERT INTO `organizations` (`id`, `title`) VALUES
+(8, 'Филиал ПАО «МРСК Северо-Запада» «Колэнерго»');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
